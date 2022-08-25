df7cbc656728ec32771ef438e0fba2ba22107c12
Pedro Dias <pedro158a@gmail.com>
2022-08-25 19:43:38 +0100

    Initial commit

---
+++ src/test/java/com/korber/dto/DtoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.korber.dto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DtoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
