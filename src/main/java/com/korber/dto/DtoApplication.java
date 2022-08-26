3438a895f837841e97135f9fe50750e08b9528f7
Pedro Dias <pedro158a@gmail.com>
2022-08-27 00:17:39 +0100

    Add swagger documentation

--- src/main/java/com/korber/dto/DtoApplication.java
+++ src/main/java/com/korber/dto/DtoApplication.java
@@ -1,8 +1,12 @@
 package com.korber.dto;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@Slf4j
+@EnableSwagger2
 @SpringBootApplication
 public class DtoApplication {
 
