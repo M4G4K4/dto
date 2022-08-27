ccdb72e3546cf703a2f06dca7c31090e87b3b833
Pedro Dias <pedro158a@gmail.com>
2022-08-28 00:51:18 +0100

    Implement integration tests.

---
+++ src/test/java/com/korber/dto/BaseIT.java
@@ -0,0 +1,49 @@
+package com.korber.dto;
+
+import io.restassured.RestAssured;
+import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.web.ServerProperties;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@SpringBootTest(classes = DtoApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("it")
+@Testcontainers
+public abstract class BaseIT {
+
+    @LocalServerPort
+    int port;
+
+    @BeforeEach
+    public void setUp() {
+        RestAssured.port = port;
+        RestAssured.baseURI = "http://localhost:" + port;
+    }
+    @Container
+    private static final PostgreSQLContainer postgresContainer = new PostgreSQLContainer("postgres:latest");
+
+
+    @DynamicPropertySource
+    public static void setDatasourceProperties(final DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgresContainer::getJdbcUrl);
+        registry.add("spring.datasource.username", postgresContainer::getUsername);
+        registry.add("spring.datasource.password", postgresContainer::getPassword);
+    }
+
+    @Test
+    void testContainers() {
+        Assertions.assertTrue(postgresContainer.isRunning());
+    }
+
+}
+
--- src/test/java/com/korber/dto/trip/TripIT.java
+++ src/test/java/com/korber/dto/trip/TripIT.java
@@ -1,20 +1,53 @@
 package com.korber.dto.trip;
 
-import com.korber.dto.service.TripService;
+import com.korber.dto.BaseIT;
+import com.korber.dto.model.comom.TripType;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 
-@SpringBootTest
-public class TripIT {
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
 
-    @Autowired
-    TripService service;
-
+public class TripIT extends BaseIT {
 
     @Test
     void getYellowList() {
+        given()
+                .get("/trip/list-yellow")
+                .then()
+                .statusCode(HttpStatus.OK.value())
+                .body("items[0].id", equalTo(200),
+                        "items[0].pickupAt", notNullValue(),
+                        "items[0].dropOffAt", notNullValue(),
+                        "items[0].pickUpZone", equalTo("East Village"),
+                        "items[0].dropOffZone", equalTo("UN/Turtle Bay South"),
+                        "items[0].type", equalTo(TripType.YELLOW.name()));
+    }
 
+    @Test
+    void getYellowListQueryParameterDefaultValues() {
+        given()
+                .get("/trip/list-yellow")
+                .then()
+                .statusCode(HttpStatus.OK.value())
+                .body("page", equalTo(1),
+                        "perPage", equalTo(10),
+                        "total", equalTo(100),
+                        "totalPages", equalTo(10),
+                        "items", hasSize(10));
+    }
+
+    @Test
+    void getYellowListQueryParameterUserValues() {
+        given()
+                .get("/trip/list-yellow?page=5&perPage=20&order=id")
+                .then()
+                .statusCode(HttpStatus.OK.value())
+                .body("page", equalTo(5),
+                        "perPage", equalTo(20),
+                        "total", equalTo(100),
+                        "totalPages", equalTo(5),
+                        "items", hasSize(20));
     }
 
 }
