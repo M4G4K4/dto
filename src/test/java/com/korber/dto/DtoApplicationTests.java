c0280d006c1e8af1b96c71a0c58a320142e53cb4
Pedro Dias <pedro158a@gmail.com>
2022-08-27 22:37:05 +0100

    Start testing implementation.

--- src/test/java/com/korber/dto/DtoApplicationTests.java
+++ src/test/java/com/korber/dto/DtoApplicationTests.java
@@ -1,13 +1,12 @@
 package com.korber.dto;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class DtoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+
+    }
 
 }
---
+++ src/test/java/com/korber/dto/trip/TripIT.java
@@ -0,0 +1,20 @@
+package com.korber.dto.trip;
+
+import com.korber.dto.service.TripService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class TripIT {
+
+    @Autowired
+    TripService service;
+
+
+    @Test
+    void getYellowList() {
+
+    }
+
+}
