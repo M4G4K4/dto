34314b4abdc9d385e7f40ca39b5451272ba899b8
Pedro Dias <pedro158a@gmail.com>
2022-08-27 18:54:43 +0100

    Implement list yellow resource.

--- src/main/java/com/korber/dto/service/TripService.java
+++ src/main/java/com/korber/dto/service/TripService.java
@@ -1,18 +1,44 @@
 package com.korber.dto.service;
 
-import com.korber.dto.model.data.ZoneRepository;
+import com.korber.dto.model.data.TripRepository;
+import com.korber.dto.model.dto.trip.TripList;
+import com.korber.dto.model.dto.trip.TripRead;
+import com.korber.dto.model.jpa.Trip;
+import com.korber.dto.model.mapper.TripMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @Slf4j
 public class TripService {
 
     @Autowired
-    protected ZoneRepository repository;
+    protected TripMapper mapper;
 
-    public void someMethod() {
+    @Autowired
+    protected TripRepository repository;
 
+    public TripList getYellowTrips(final Integer page, final Integer perPage, final String order) {
+
+        Pageable pageable = PageRequest.of(page - 1, perPage, Sort.by(order).descending());
+
+        final Page<Trip> tripList = repository.findAll(pageable);
+
+        final List<TripRead> tripReadList = tripList.get().map(mapper::tripToTripRead).toList();
+
+        return TripList.builder()
+                .perPage(perPage)
+                .page(page)
+                .total(tripList.getTotalElements())
+                .totalPages(tripList.getTotalPages())
+                .items(tripReadList)
+                .build();
     }
 }
--- src/main/java/com/korber/dto/web/TripController.java
+++ src/main/java/com/korber/dto/web/TripController.java
@@ -1,15 +1,25 @@
 package com.korber.dto.web;
 
+import com.korber.dto.model.dto.trip.TripList;
+import com.korber.dto.service.TripService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.ws.rs.QueryParam;
+
 @RestController
+@RequestMapping("/trip")
 public class TripController {
 
+    @Autowired
+    private TripService service;
+
     @GetMapping("/list-yellow")
-    public ResponseEntity<String> topZones() {
-        return new ResponseEntity<>("list-yellow", HttpStatus.OK);
+    public ResponseEntity<TripList> topZones(@QueryParam("page") final Integer page, @QueryParam("perPage") final Integer perPage, @QueryParam("order") final String order) {
+        return new ResponseEntity<>(service.getYellowTrips(page, perPage, order), HttpStatus.OK);
     }
 }
